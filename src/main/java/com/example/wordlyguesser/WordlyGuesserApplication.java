89c29f95c21cbbc053b56e01bbeab716b956a956
popkov <dmitrijp8@gmail.com>
2022-03-22 14:28:52 +0300

    fix1

--- src/main/java/com/example/wordlyguesser/WordlyGuesserApplication.java
+++ src/main/java/com/example/wordlyguesser/WordlyGuesserApplication.java
@@ -2,13 +2,12 @@ package com.example.wordlyguesser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 @SpringBootApplication
-public class WordlyGuesserApplication {
+public class WordlyGuesserApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) throws IOException {
         SpringApplication.run(WordlyGuesserApplication.class, args);
